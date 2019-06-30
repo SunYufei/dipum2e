93a2013f791e370bdb5a6c30b654534d97668826
SunYufei <sunyufei963@gmail.com>
2019-06-30 17:20:20 +0800

    Update: 2019/06/30

---
+++ commit.bat
@@ -0,0 +1,3 @@
+git add .
+git commit -m "Update: %date:~0,10%"
+git push origin master
--- commit.ps1
+++
@@ -1,6 +0,0 @@
-$today = Get-Date
-$commit = "Update: {0}" -f $today.ToString('yyyy/MM/dd')
-
-git.exe add .
-git.exe commit -m $commit
-git.exe push origin master
